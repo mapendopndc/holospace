a80abdb18f56199e7dea6c4658142caf382ff570
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-18 18:07:13 -0400

    Add api routes and auth

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-app.use(express.static('models'))
+const userRoutes = require('./api/routes/user');
 
-app.get('/', (req, res) => res.send('Holospace web interface in development.'))
+mongoose.connect('mongodb+srv://holospace-app:' + process.env.MONGO_ATLAS_PW + '@holospace-app-nc4b7.mongodb.net/holospace-app?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static('models'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+app.use('/user', userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
