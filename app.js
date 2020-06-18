263e4698637e7e8ecd3a8ce8d7c7865f6a1b4a10
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-17 22:45:28 -0400

    Add 3D models for testing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello Nico and Dixon hehe.'))
+app.use(express.static('models'))
+
+app.get('/', (req, res) => res.send('Holospace web interface in development.'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
