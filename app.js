c1c3ec3d1d876843d9f0b445215ef4c4019845b6
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-18 18:34:34 -0400

    Add JWT signature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
 const app = express();
 const port = 3000;
 
