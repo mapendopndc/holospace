1e7ccb56fa7fcbc8b8864ddeaf946165133b5f74
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-19 01:20:53 -0400

    Modify app.js to console log env variable

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = 3000;
 
 const userRoutes = require('./api/routes/user');
 
+console.log(process.env.MONGO_ATLAS_PW);
+
 mongoose.connect('mongodb+srv://holospace-app:' + process.env.MONGO_ATLAS_PW + '@holospace-app-nc4b7.mongodb.net/holospace-app?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
