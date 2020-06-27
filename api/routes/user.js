cde127f016cc82dee9dbbb79d7b72c5edc259ba0
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-27 08:40:52 -0400

    Add user ID to login response

--- api/routes/user.js
+++ api/routes/user.js
@@ -80,7 +80,8 @@ router.post('/login', (req, res, next) => {
                     );
                     return res.status(200).json({
                         message: 'Auth successful',
-                        token: token
+                        token: token,
+                        user_id: user.id
                     });
                 }
                 res.status(401).json({
